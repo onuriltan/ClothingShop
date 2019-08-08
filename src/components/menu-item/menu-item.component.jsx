15f3a497f0cad719e138e4287bf607acd25ad53f
onuriltan <onur.iltan@gmail.com>
2019-08-08 17:26:06 +0300

    make size bigger in some menu items accroding to prop value

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div className='menu-item' style={{ backgroundImage: `url(${imageUrl})` }}>
+    <div className={`${size} menu-item`} style={{ backgroundImage: `url(${imageUrl})` }}>
       <div className='content'>
         <h1 className='title'>{title}</h1>
         <span className='subtitle'>SHOP NOW</span>
