eb994415904008d547bfa58e995d4f68bc28025d
onuriltan <onur.iltan@gmail.com>
2019-08-15 15:00:27 +0300

    add directory redux flow to reuse the state of directory across the app

--- src/components/directory/Directiory.jsx
+++ src/components/directory/Directiory.jsx
@@ -1,60 +1,27 @@
 import React from 'react'
 import './directory.scss'
-
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { selectDirectorySection } from '../../redux/directory/directory.selectors'
+import { createStructuredSelector } from 'reselect'
 import MenuItem from '../menu-item/MenuItem'
 
-class Directory extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      sections: [
-        {
-          title: 'HATS',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'JACKETS',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'SNEAKERS',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'WOMEN',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/women'
-        },
-        {
-          title: 'MEN',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/men'
-        }
-      ]
-    }
-  }
-
-  render () {
-    const {sections} = this.state
-    return (
-      <div className='directory-menu'>
-        {sections && sections.map(({id, ...otherSectionProps}) => {
-          return <MenuItem key={id} {...otherSectionProps} />
-        })}
-      </div>
-    )
-  }
+const Directory = ({ sections }) => {
+  return (
+    <div className='directory-menu'>
+      {sections && sections.map(({id, ...otherSectionProps}) => {
+        return <MenuItem key={id} {...otherSectionProps} />
+      })}
+    </div>
+  )
 }
 
-export default Directory
+Directory.propTypes = {
+  sections: PropTypes.array.isRequired
+}
+
+const mapStateToProps = () => createStructuredSelector({
+  sections: selectDirectorySection
+})
+
+export default connect(mapStateToProps, null)(Directory)
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from 'react'
 import './home_page.scss'
 
-import Directory from "../../components/directory/Directiory";
+import Directory from '../../components/directory/Directiory'
 
 const Home = () => {
   return (
     <div className='homepage'>
       <Directory />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
