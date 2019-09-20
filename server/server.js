56ab489df9dd64433b1e3a09a5f9963626cc27b9
onuriltan <onur.iltan@gmail.com>
2019-09-20 17:52:08 +0300

    fix deployment

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true} ))
 app.use(cors())
 
-if(process.env.NODE_ENV !== 'production') {
+if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')))
   app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
@@ -22,5 +22,9 @@ if(process.env.NODE_ENV !== 'production') {
 app.listen(port, error => {
   if (error) throw error
   console.log(`Clothing Shop Server running on port ${port}`)
+})
+
+app.post('/payment', (req, res) => {
+
 })
 
