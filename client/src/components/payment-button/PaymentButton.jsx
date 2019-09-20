2ce436650c8477efd907b0e8f305a2aaa1776a5e
onuriltan <onur.iltan@gmail.com>
2019-09-20 18:29:50 +0300

    add stripe payment integration to react ui

--- client/src/components/payment-button/PaymentButton.jsx
+++ client/src/components/payment-button/PaymentButton.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const PaymentButton = ({ price }) => {
   const priceInCents = price * 100
   const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 
   const onToken = token => {
-    // TODO: pass token to backend to create charge
-    // eslint-disable-next-line no-undef
-    alert(process.env.REACT_APP_PAYMENT_SUCCESSFUL_MESSAGE)
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceInCents,
+        token
+      }
+    }).then(res => {
+      window.alert('Payment is successful!')
+    }).catch(err => {
+      console.log('Payment error: ' + JSON.parse(err))
+      window.alert('Payment is unsuccessful :( Make sure you provided correct credit card info')
+    })
     console.log(token)
   }
 
--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('express')
 const path = require('path')
-
 if(process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -25,6 +26,21 @@ app.listen(port, error => {
 })
 
 app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  }
+
+  stripe.charges.create(body, (stripeError, stripeRes ) => {
+    if(stripeError) {
+      res.status(500).send({ error: stripeError })
+    }
+    else {
+      res.status(200).send({ success: stripeRes })
+    }
+
+  })
 
 })
 
