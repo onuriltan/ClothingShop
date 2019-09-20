1257714f5925bcfac14eb59d2ae06ae4d95c72d7
onuriltan <onur.iltan@gmail.com>
2019-09-20 17:37:37 +0300

    update build folder

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true} ))
 app.use(cors())
 
 if(process.env.NODE_ENV !== 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, '../client/build')))
   app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
   })
 }
 
